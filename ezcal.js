f9617b5585a944dddd5e0e7e6560f523fec24a1e
trevor-shepard <trevorbshepard@gmail.com>
2019-02-07 09:59:03 -0700

    Pain Days and Paint Months into weeks

--- ezcal.js
+++ ezcal.js
@@ -23,19 +23,66 @@ class CssRule  {
 
 
 class Day {
-    constructor(date) {
+    constructor(year, month, day) {
+        this.month = month
+        this.day = day
+        this.year = year
+        this.date = new Date(year, month, day)
         
+    }
+
+    paintDay() {
+        this.el = document.createElement('div')
+        this.el.classList.add("day")
+        this.el.id = `${this.month}-${this.day}-${this.year}`
+        this.el.innerText = this.day
+        return this.el
     }
 
 }
 
 class Month {
     constructor(date) {
-        
+        // find start of month
         this.startofMonth = new Date(date.getFullYear(), date.getMonth(), 1)
-        this.setVariables.call(this)
-        this.endofMonth = new Date(date.getFullYear(), date.getMonth(), this.daysInMonths[date.getMonth()])
 
+        // determine leap year and number of days in month
+        this.setVariables.call(this)
+
+        // find last day of month
+        this.endofMonth = new Date(date.getFullYear(), date.getMonth(), this.daysInMonths[date.getMonth()])
+        
+        // find all current month days and lapover days from pervious and next month to be displayed
+        this.generateDays.call(this, date)
+
+        // paint calender onto a div with the id "ez-cal"
+        this.paintMonth = this.paintMonth.bind(this)
+    }
+
+    paintMonth() {
+        // create month element
+        this.el = document.createElement('div')
+        this.el.classList.add("month")
+        this.el.id = `${this.currMonth}-${this.currYear}`
+        let week
+        let day
+        // create weeks
+        for (let wIndex = 0; wIndex < (this.days.length / 7); wIndex++) {
+            week = document.createElement('div')
+            week.classList.add(`week`)
+            for (let dIndex = 0; dIndex < 7; dIndex++) {
+                day = this.days[(7 * wIndex) + dIndex]
+                console.log(day)
+                week.appendChild(day.paintDay()) 
+            }
+            this.el.appendChild(week)
+        }
+        
+        return this.el
+        
+    }
+
+    generateDays(date) {
         // account for year rollover
         this.lastMonth = date.getMonth() === 0 ? 11 : date.getMonth() - 1
         this.lastMonthYear =  date.getMonth() === 0 ? (date.getFullYear - 1) : date.getFullYear()
@@ -45,21 +92,25 @@ class Month {
         this.days = []
         // Get previous month lapover days
         for (let index = 0; index < this.startofMonth.getDay(); index++) {
-            this.days.unshift(new Date(this.lastMonthYear, this.lastMonth, this.daysInMonths[this.lastMonth] - index))
+            this.days.unshift(new Day(this.lastMonthYear, this.lastMonth, this.daysInMonths[this.lastMonth] - index))
         }
         // Get current months days
         for (let index = 1; index < (this.daysInMonths[date.getMonth()] + 1); index++) {
-            this.days.push(new Date(date.getFullYear(), date.getMonth(), index))
+            this.days.push(new Day(date.getFullYear(), date.getMonth(), index))
         }
         // Get next month lapover days
         
         for (let index = 0; index < 6 - (this.endofMonth.getDay()); index++) {
-            this.days.push(new Date(this.nextMonthYear, this.nextMonth, index + 1))
+            this.days.push(new Day(this.nextMonthYear, this.nextMonth, index + 1))
         }
     }
 
+
+
     setVariables() {
-        let currYear = this.startofMonth.getFullYear()
+        this.currYear = this.startofMonth.getFullYear()
+        
+        this.currMonth = this.startofMonth.getMonth()
 
         this.daysInMonths = {
             0: 31,
@@ -77,7 +128,7 @@ class Month {
         }
 
         // Check for leap year
-        if ((!(currYear % 4) && (currYear % 100)) || !(currYear % 400)) {
+        if ((!(this.currYear % 4) && (this.currYear % 100)) || !(this.currYear % 400)) {
             this.daysInMonths[1] = 29
         }
         
@@ -104,8 +155,8 @@ class Calender {
     constructor(el) {
         this.el = el
         this.today = new Date()
-        
         this.currMonth = new Month(this.today)
+        this.el.appendChild(this.currMonth.paintMonth())
     }
 
     
